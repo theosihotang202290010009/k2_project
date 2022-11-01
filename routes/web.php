164dd401fed3c23c62f8c3ccad3e723dd75539bd
willigithy2002 <116245078+willigithy2002@users.noreply.github.com>
2022-11-02 00:40:43 +0700

    add route for keranjang

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', [App\Http\Livewire\Home::class, 'render'])->name('home');
-Route::get('/products', [App\Http\Livewire\ProductIndex::class, 'render'])->name('products');
-Route::get('/products/{id}', [App\Http\Livewire\ProductDetail::class, 'render'])->name('products.detail');
-
+Route::get('/', App\Http\Livewire\Home::class)->name('home');
+Route::get('/products', App\Http\Livewire\ProductIndex::class)->name('products');
+Route::get('/products/{id}', App\Http\Livewire\ProdukDetail::class)->name('products.detail');
+Route::get('/keranjang', App\Http\Livewire\Keranjang::class)->name('keranjang');
