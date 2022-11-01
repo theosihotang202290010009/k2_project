33e45d4b36464c450055c8cf000aa5a27924da7c
willigithy2002 <116245078+willigithy2002@users.noreply.github.com>
2022-11-01 23:59:54 +0700

    Change to English for Stock Ready String

--- resources/views/livewire/product-detail.blade.php
+++ resources/views/livewire/product-detail.blade.php
@@ -16,11 +16,23 @@
         </div>
     </div>
 
-    <div class="row mt-3">
-        <div class="col-md-6">
-            <div class="card">
-                <img src="{{ url('assets/produk') }}/{{ $produk->gambar }}" class="img-fluid">
+    <div class="row">
+            <div class="col-md-6">
+                <div class="card gambar-produk">
+                    <div class="card-body">
+                        <img src="{{ url('assets/produk') }}/{{ $produk->gambar }}" class="img-fluid">
+                    </div>
+                </div>
             </div>
-        </div>
+            <div class="col-md-6">
+                <h2><strong>{{ $produk->nama_pdk }}</strong>
+                    @if($produk->ready_or_not == 1)
+                <span class="badge badge-success"> <i class="fas fa-check"></i> Ready to Stock</span>
+                @else
+                <span class="badge badge-danger"> <i class="fas fa-times"></i> Out of Stock</span>
+                @endif
+                <h4>Rp. {{ number_format($produk->harga) }}</h4>
+                <hr>
+            </div>
     </div>
 @endsection
