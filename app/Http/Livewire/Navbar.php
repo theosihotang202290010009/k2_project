38f3ba6b8c37a447c09fdc0ccd260e9b15faf1e4
willigithy2002 <116245078+willigithy2002@users.noreply.github.com>
2022-11-02 00:25:06 +0700

    Update Navbar.php

--- app/Http/Livewire/Navbar.php
+++ app/Http/Livewire/Navbar.php
@@ -2,12 +2,45 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Auth;
+use App\Models\Produk;
+use App\Models\Pesanan;
 use Livewire\Component;
 
 class Navbar extends Component
 {
+    public $amount = 0;
+
+    protected $listeners = [
+        'masukKeranjang' => 'updateKeranjang'
+    ];
+
+    public function updateKeranjang(){
+
+        if(Auth::user()){
+            $pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status', 0)->first();
+            if ($pesanan) { 
+                $this->amount = PesananDetail::where('pesanan_id', $pesanan->id)->count();
+            }
+        }    
+    }
+
+    public function mount(){
+
+        if(Auth::user()){
+            $pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status', 0)->first();
+            if ($pesanan) { 
+                $this->amount = PesananDetail::where('pesanan_id', $pesanan->id)->count();
+            }
+        }
+    }
+
     public function render()
     {
-        return view('livewire.navbar');
+        $produks = Produk::all();
+        return view('livewire.navbar', [
+            'produks' => $produks,
+            'jumlah_pesanan' => $this->amount,
+        ]);
     }
 }
