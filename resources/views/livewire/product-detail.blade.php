004692b65fe54623837c6bdb2167595877965e0f
willigithy2002 <116245078+willigithy2002@users.noreply.github.com>
2022-11-02 00:02:57 +0700

    change product-detail string for badge
    "Ready to stock" badge instead of "Stock Ready"

--- resources/views/livewire/product-detail.blade.php
+++ resources/views/livewire/product-detail.blade.php
@@ -1,22 +1,19 @@
 @extends('layouts.app')
 @section('content')
-<div>
-    <h2>Product Details</h2>
-</div>
 <div class="container">
-    <div class="row mb-2">
+    <div class="row mt-4 mb-2">
         <div class="col">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="{{ route('home') }}" class="text-dark">Home</a></li>
                     <li class="breadcrumb-item"><a href="{{ route('products') }}" class="text-dark">List Bakery</a></li>
-                    <li class="breadcrumb-item-active" aria-current="page">Product Detail</li>
+                    <li class="breadcrumb-item active" aria-current="page"><strong>Bakery Detail</strong></li>
                 </ol>
             </nav>
         </div>
     </div>
 
-     <div class="row">
+    <div class="row">
             <div class="col-md-6">
                 <div class="card gambar-produk">
                     <div class="card-body">
